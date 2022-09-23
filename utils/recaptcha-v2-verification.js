e01793e8248f65f4c7c03b751280f8359d73e057
Fadil M <ext.Fadil.Muca@dmz.skoda-auto.cz>
2022-09-23 12:37:11 +0200

    updated secret key variable

--- utils/recaptcha-v2-verification.js
+++ utils/recaptcha-v2-verification.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 export const recaptchaV2Verification = async (recaptchaKey) => {
-    console.log('AA: ', process.env.GOOGLE_RECAPTCHA_V2_LOCALHOST_SECRET_KEY);
+    console.log('AA: ', process.env.GOOGLE_RECAPTCHA_V2_SECRET_KEY);
     console.log('token: ', recaptchaKey);
     const validation = await axios
-        .post(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.GOOGLE_RECAPTCHA_V2_LOCALHOST_SECRET_KEY}&response=${recaptchaKey}`, {}, {})
+        .post(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.GOOGLE_RECAPTCHA_V2_SECRET_KEY}&response=${recaptchaKey}`, {}, {})
         .then(async (res) => {
             return await res.data;
         }).catch(async (err) => {
