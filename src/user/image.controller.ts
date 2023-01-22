e7bf691850c61e22a356fe6ef36648e63c048035
fudeel <fadil_mucia@hotmail.com>
2023-01-22 14:21:40 +0100

    set maximum size for image to write in db

--- src/user/image.controller.ts
+++ src/user/image.controller.ts
@@ -5,7 +5,20 @@ import {CustomResponse} from "../../models/CustomResponse";
 import * as mongoose from "mongoose";
 
 const uploadProfilePictureSchema = Joi.object().keys({
-    pic: Joi.string().required(),
+    pic: Joi.string().required().custom((value, helpers) => {
+        const buffer = Buffer.from(value, 'base64');
+        if (buffer.length > 2100000) {
+            return helpers.error('string.base64ImageSize');
+        }
+        return value;
+    }).error((errors: any) => {
+        errors.forEach((err) => {
+            if (err.type === 'string.base64ImageSize') {
+                err.message = `Base64 image exceeded the maximum size of ${2100000} bytes`;
+            }
+        });
+        return errors;
+    }),
     _id: Joi.string().required()
 });
 
@@ -19,13 +32,13 @@ export const uploadProfilePicture= async (req: Request, res: Response) => {
                     if (!err) {
                         // POST validation
                         const result = await uploadProfilePictureSchema.validate({pic: req.body.picUrl, _id: docs[0].userId});
-
                         if (result.error) {
-                            throw<CustomResponse> {
+                            console.log('Error in uploading user picture: ', err);
+                            res.status(500).send(<CustomResponse>{
                                 error: true,
-                                message: result.error.message.toString(),
+                                message: 'The image is exceeding size limit. Maximum size is 2MB',
                                 status: 500
-                            }
+                            });
                         } else {
                             const update = { pic: req.body.picUrl };
                             //const filter = { userId: docs[0].userId };
